ee61702ffeb93d8d2fca0aebc15d9f228b83cb12
TyFB <tfb1223@gmail.com>
2022-10-12 07:20:21 -0500

    finale touches

--- app.js
+++ app.js
@@ -92,6 +92,24 @@ const promptProject = portfolioData => {
         },
         {
             type: 'confirm',
+            name: 'confirmAbout',
+            message: 'Would you like to enter some information about yourself for an "About" section?',
+            default: true
+        },
+        {
+            type: 'input',
+            name: 'about',
+            message: 'Provide some information about yourself:',
+            when: ({ confirmAbout }) => {
+                if (confirmAbout) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'confirm',
             name: 'feature',
             message: 'Would you like to feature this project?',
             default: false
